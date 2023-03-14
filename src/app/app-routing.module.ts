124bda0fb15c9fab5f9b9cad5b08c107311081f1
Jamal Saleh <jamal.ngu@gmail.com>
2023-03-14 23:32:32 +0200

    fix netlify deployment issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: 'orders', loadChildren: () => import('./modules/orders/orders.module').then(m => m.OrdersModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
